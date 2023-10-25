e3345e0ba62d0be4d15b7f898bb29e893ee3faea
Shub-24 <141795852+Shub-24@users.noreply.github.com>
2023-10-25 21:48:17 +1100

    Add files via upload

--- BirdMovement.cs
+++ BirdMovement.cs
@@ -1,33 +1,42 @@
 
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using UnityEngine;
 
 public class BirdMovement : MonoBehaviour
 {
     public Transform[] waypoints;
     private Transform currentWaypoint;
     private int currentIndex;
+    private SpriteRenderer spriteRenderer; // Added SpriteRenderer
 
     void Start()
     {
-        currentIndex = UnityEngine.Random.Range(0, waypoints.Length); 
+        currentIndex = Random.Range(0, waypoints.Length);
         currentWaypoint = waypoints[currentIndex];
         transform.position = currentWaypoint.position;
-        
+        spriteRenderer = GetComponent<SpriteRenderer>(); // Get the SpriteRenderer component
     }
 
     void Update()
     {
         if (Vector2.Distance(transform.position, currentWaypoint.position) < 0.1f)
         {
-            currentIndex = UnityEngine.Random.Range(0, waypoints.Length); 
+            currentIndex = UnityEngine.Random.Range(0, waypoints.Length);
             currentWaypoint = waypoints[currentIndex];
-            
         }
 
         float step = 7f * Time.deltaTime;
         transform.position = Vector2.MoveTowards(transform.position, currentWaypoint.position, step);
+
+        // Flip the sprite based on movement direction
+        if (transform.position.x < currentWaypoint.position.x)
+        {
+            spriteRenderer.flipX = false; // Face right
+        }
+        else if (transform.position.x > currentWaypoint.position.x)
+        {
+            spriteRenderer.flipX = true; // Face left
+        }
     }
 }
